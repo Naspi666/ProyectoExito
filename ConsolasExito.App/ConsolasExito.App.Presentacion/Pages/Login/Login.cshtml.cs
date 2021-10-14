976542d1cda2558a53bb542975bc7ad3ef0b4cef
williamvargas7 <86165574+williamvargas7@users.noreply.github.com>
2021-10-13 21:35:43 -0500

    Pagina Login V2

--- ConsolasExito.App/ConsolasExito.App.Presentacion/Pages/Login/Login.cshtml.cs
+++ ConsolasExito.App/ConsolasExito.App.Presentacion/Pages/Login/Login.cshtml.cs
@@ -4,13 +4,54 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ConsolasExito.App.Dominio;
+using ConsolasExito.App.Persistencia;
 
 namespace ConsolasExito.App.Presentacion.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public string Usuario { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        [BindProperty]
+        public string MensajeUsuario { get; set; }
+        [BindProperty]
+        public string MensajePassword { get; set; }
+
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            Conexion conexion = new Conexion();
+            Empleado empleado = conexion.Empleados.FirstOrDefault(e => e.Usuario == Usuario);
+
+            if (empleado != null)
+            {
+                if (empleado.Password.Equals(Password))
+                {
+                    return RedirectToPage("../CrudConsola/Index");
+                }
+                else
+                {
+                    MensajePassword = "La contraseña no es correcta";
+                    return Page();
+                }
+
+            }
+            else
+            {
+                MensajeUsuario = "El usuario no ha sido encontrado";
+                return Page();
+            }
+
+
+        }
+
     }
 }
